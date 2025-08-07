6819a12c289e347b9a39b5908214548fe575158a
sabique2003 <159254150+sabique2003@users.noreply.github.com>
2025-08-07 12:41:12 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
-// src/api/api.js
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tasklite-backend-6ba9.onrender.com', // Adjust if your backend route is different
+  baseURL: 'https://tasklite-backend-6ba9.onrender.com'
 });
 
 export default api;
